7d5b70e657be9729ed5343a4a25ca1e89d812cfa
Cole Peaden <cep42@students.uwf.edu>
2017-11-22 12:44:34 -0600

    Adding functionality.

---
+++ Login/LoginController.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Login;
+
+/**
+ *
+ * @author cpeaden
+ */
+public class LoginController {
+    private int userID;
+    
+    public int getUserID(int newUserID)
+    {
+        newUserID = userID;
+        
+        //figure out how to connect GUI button here.
+        
+    }
+}
