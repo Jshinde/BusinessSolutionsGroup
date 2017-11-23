cc35efaf6f39cba9d62fd0f255097b91b6b0e82c
Goose1909 <cep42@students.uwf.edu>
2017-11-22 19:20:36 -0600

    Started to add login fiunctionality.

--- Database/DBMgr.java
+++ Database/DBMgr.java
@@ -5,6 +5,7 @@
  */
 package Database;
 import java.sql.*;
+import Login.LoginController;
 
 /**
  *
@@ -33,20 +34,28 @@ public class DBMgr
     * as a real ID_NUM, then the user is let into the program.
     * 
     * If not, the user will be asked to try again or exit.
+    * 
+    * NON FUNCTIONAL AT THE TIME 11/22/17
+    * 
     */
    public void loginToProgram(int ID_NUM) throws SQLException
    {
         conn = DriverManager.getConnection("jdbc:derby://localhost:1527/BusinessSolutions", "person", "password");
         System.out.println("Connection Created");
         
-        if (stat.execute("SELECT * FROM EMPLOYEE WHERE ID_NUM =" + ID_NUM))
+        if (stat.execute("SELECT ID_NUM FROM EMPLOYEE WHERE ID_NUM = " + ID_NUM))
         {
             //login program code to enter program
+            LoginController lc = new LoginController();
+            
+            lc.getUserID(ID_NUM);
+            System.out.println("SUCCESS");
         }
         
         else
         {
             //login code to exit program or try again.
+            System.out.println("ERROR");
         }
    }
 }
