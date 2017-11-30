b830b7dc6dc2dd62c729ce0b20721fc736b58b0f
Goose1909 <cep42@students.uwf.edu>
2017-11-30 13:50:14 -0600

    Made Login GUI open TaskTicketGUI

--- Login/LoginController.java
+++ Login/LoginController.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package Login;
+import Ticketing.TaskTicketGUI;
 import Database.DBMgr;
 import java.sql.SQLException;
 
@@ -12,13 +13,26 @@ import java.sql.SQLException;
  * @author cpeaden
  */
 public class LoginController {
-    private int userID;
+   /* private int userID;
+    DBMgr temp = null;
     
     public void getUserID(int newUserID) throws SQLException
     {
         newUserID = userID;
-        DBMgr temp = new DBMgr();
-        
         temp.loginToProgram(userID);
+    }*/
+    
+    public void login(int userID)
+    {
+       if (userID == 123456)
+       {
+           TaskTicketGUI task = new TaskTicketGUI();
+           task.setVisible(true);
+           
+       }
+    }
+
+    public void getUserID(int ID_NUM) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
