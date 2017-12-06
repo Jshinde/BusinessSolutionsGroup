5117884dc877818cd6724755a4677683f2095f60
Goose1909 <cep42@students.uwf.edu>
2017-12-05 22:43:03 -0600

    Added to package.

--- Scheduler/SchedulerController.java
+++ Scheduler/SchedulerController.java
@@ -1,7 +1,5 @@
 package Scheduler;
 
-import javax.swing.JFrame;
-
 public class SchedulerController 
 {
 	public void schedulePerson(Object personToSchedule, String dayToSchedule) 
@@ -9,10 +7,9 @@ public class SchedulerController
 		//Add person to day
 	}
 	
-	public static void main() {
-		SchedulerGUI sgui = new SchedulerGUI();
-		sgui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		sgui.setSize(275,180);
-		sgui.setVisible(true);
+	public void startNewWeek() 
+	{
+		//If permissions accepted
+		//Clear the week
 	}
 }
