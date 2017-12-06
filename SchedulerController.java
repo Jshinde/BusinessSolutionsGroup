a39e2377fbe2ca98c58ca1d3117d17a8f0d14fbe
Oren Powell <32081044+orenpowell@users.noreply.github.com>
2017-12-05 22:36:00 -0600

    Updated GUI

---
+++ SchedulerController.java
@@ -0,0 +1,15 @@
+package scheduler;
+
+public class SchedulerController 
+{
+	public void schedulePerson(Object personToSchedule, String dayToSchedule) 
+	{
+		//Add person to day
+	}
+	
+	public void startNewWeek() 
+	{
+		//If permissions accepted
+		//Clear the week
+	}
+}
